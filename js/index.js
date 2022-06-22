3e372f1e4b0e371b57837ff661931d4af0de5cdf
Twentyquid <negativeradiations@gmail.com>
2022-06-22 21:49:45 +0530

    added some animations on load

---
+++ js/index.js
@@ -0,0 +1,47 @@
+let logo = document.querySelectorAll(".logo");
+let navlinks = document.querySelectorAll(".nav-links");
+let heroimage = document.querySelectorAll(".hero-image");
+
+document.addEventListener("DOMContentLoaded", () => {
+  anime({
+    targets: logo,
+    translateY: [-10, 0],
+    easing: "easeOutExpo",
+    opacity: [0.2, 1],
+  });
+
+  anime({
+    targets: navlinks,
+    translateY: [-20, 0],
+    easing: "easeOutExpo",
+    opacity: [0, 1],
+  });
+
+  anime({
+    targets: heroimage,
+    easing: "easeOutExpo",
+    opacity: [0, 1],
+    delay: 1100,
+  });
+
+  anime({
+    targets: ".bar1",
+    width: ["0px", "30px"],
+    easing: "easeOutExpo",
+    delay: 1050,
+  });
+
+  anime({
+    targets: ".bar2",
+    width: ["0px", "25px"],
+    easing: "easeOutExpo",
+    delay: 1100,
+  });
+
+  anime({
+    targets: ".bar3",
+    width: ["0px", "20px"],
+    easing: "easeOutExpo",
+    delay: 1150,
+  });
+});
